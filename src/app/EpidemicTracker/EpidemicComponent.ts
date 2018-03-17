b18ce6917a4ab5ea477c0f953d4fa3e00297a0f9
Brandon Horn <brandonhorn09@gmail.com>
2018-03-17 13:35:12 -0400

    quick push epi

--- src/app/EpidemicTracker/EpidemicComponent.ts
+++ src/app/EpidemicTracker/EpidemicComponent.ts
@@ -1,6 +1,21 @@
 import { Component, Input, EventEmitter } from '@angular/core';
 import { IInfectionCard } from '../InfectionDeck/interfaces';
 
+export interface IPlayerCard {
+    name: string;
+    color: string;
+}
+
+interface IEpiMiniStack {
+    hasEpidemic: boolean;
+    count: number;
+}
+
+const EpiCardSingleton: IPlayerCard = {
+    name: 'epidemic',
+    color: 'awful'
+}
+
 @Component({
   selector: 'epidemic-tracker',
   templateUrl: './EpidemicComponent.html',
@@ -9,9 +24,28 @@ import { IInfectionCard } from '../InfectionDeck/interfaces';
 export class EpidemicComponent {
     @Input() epidemicEvent: EventEmitter<void>;
 
-    public epidemic(): void {
-        if (this.epidemicEvent) {
-            this.epidemicEvent.emit();
+    public deckLength: number;
+    public epidemicCount: number;
+    public drawnStack: IPlayerCard[];
+    public stacks: IEpiMiniStack[] = [];
+    public modalVisible: boolean = false;
+
+    constructor() {
+        
+    }
+
+    public drawCard(event: {value: IPlayerCard}): void {
+        if (event.value === EpiCardSingleton) {
+            this.stacks[0].hasEpidemic = true;
+            if (this.epidemicEvent) {
+                this.epidemicEvent.emit();
+            }
+        } else {
+            this.drawnStack.push(event.value);
+        }
+        this.stacks[0].count--;
+        if (this.stacks[0].count === 0) {
+            this.stacks.pop();
         }
     }
 }
